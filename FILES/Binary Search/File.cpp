1bb5e9ffe10e58030f242741fe7abde3985054f3
Mohsin Mukhtiar Lashari <160683327+president-xd@users.noreply.github.com>
2024-07-31 20:30:38 +0500

    Update File.cpp

--- FILES/Binary Search/File.cpp
+++ FILES/Binary Search/File.cpp
@@ -3,18 +3,18 @@ using namespace std;
 
 int binarysearch(int array[5],int key)
 {
-    int start=0,end=5;
-    while(start<=end){
-        int middle=(start+end)/2;
+    int start = 0, end = 5;
+    while( start <= end){
+        int middle = (start+end) / 2;
 
-        if (array[middle]==key){
+        if (array[middle] == key){
             return middle;
         }
-        else if (array[middle]>key){
-            end=middle-1;
+        else if (array[middle] > key){
+            end = middle - 1;
         }
-        else if (array[middle]<key){
-            start=middle+1;
+        else if (array[middle] < key){
+            start = middle + 1;
         }
         else{
             return -1;
@@ -26,9 +26,9 @@ int binarysearch(int array[5],int key)
 
 int main()
 {
-    int array[5]={1,2,3,4,5},key;
-    cout<<"Enter the number to find in array : ";
-    cin>>key;
-    cout<<"The index of "<<key<<" is "<<binarysearch(array,key);
+    int array[5] = {1,2,3,4,5}, key;
+    cout << "Enter the number to find in array : ";
+    cin >> key;
+    cout << "The index of " << key << " is " << binarysearch(array, key);
     return 0;
 }
